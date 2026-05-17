85cf4b2bfb8f6d84f45330a105cefa3fde6b75c6
Lucifersk <sk8235499@gmail.com>
2026-05-18 02:14:13 +0530

    final fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,15 @@
 export default function Navbar() {
     return (
         <nav className="w-full border-b border-zinc-800 p-4 flex items-center justify-between">
+
+            <h1 className="text-2xl font-black">
+                StudyWiki AI
+            </h1>
+
             <p className="text-zinc-500 text-sm">
                 AI-powered knowledge system
             </p>
 
-            <label className="bg-white text-black px-5 py-2 rounded-xl cursor-pointer hover:scale-105 transition">
-
-                Upload Notes
-
-                <input
-                    type="file"
-                    className="hidden"
-                    accept=".txt,.md,.json"
-                />
-
-            </label>
         </nav>
     );
 }
